af4502e7f5ff7f4c944da401d9f696a03facde9e
nassir_n <nassir_n@etna-alternance.net>
2019-05-19 14:44:56 +0200

    modified

--- API/api.py
+++ API/api.py
@@ -8,25 +8,14 @@ import sys
 sys.path.insert(0, './routes')
 from team import *
 from user import *
-<<<<<<< HEAD
 from stats import *
 
 api = Api(app)
 
-=======
-
-api = Api(app)
-
-class Index(Resource):
-    def get(self, name):
-        return {'Ton nom est' : name}
-
->>>>>>> 993db78dcaa7e5683db8964e36067d7def262f6f
 class Home(Resource):
     def get(self):
         return {'message':'Welcome Friends'}
 
-<<<<<<< HEAD
 #Route for log
 
 #Routes for Team
@@ -47,19 +36,6 @@ api.add_resource(AllStatTeam, '/stat/team')
 api.add_resource(StatTeamById, '/team/stat/<int:id>')
 
 api.add_resource(Home, '/')
-=======
-#Routes for team
-api.add_resource(allTeam, '/team')
-api.add_resource(Stats, '/stat/<int:id>')
-
-#Routes for user
-api.add_resource(user, '/addUser')
-api.add_resource(AllUser, '/users')
-api.add_resource(UserStat, '/stat')
-
-api.add_resource(Home, '/')
-api.add_resource(Index, '/index/<name>')
->>>>>>> 993db78dcaa7e5683db8964e36067d7def262f6f
 
 if __name__ == '__main__':
     app.run()
--- API/routes/team.py
+++ API/routes/team.py
@@ -19,7 +19,6 @@ class createTeam(Resource):
     def post(self):
         return jsonify({'insert':'team'})
 
-<<<<<<< HEAD
 class teamById(Resource):
     def get(self, id):
         conn = mysql.connect()
@@ -35,14 +34,6 @@ class teamByName(Resource):
         conn = mysql.connect()
         cursor = conn.cursor(pymysql.cursors.DictCursor)
         cursor.execute("SELECT * FROM team WHERE name LIKE %s", name+'%')
-=======
-class Stats(Resource):
-    def get(self, id):
-        conn = mysql.connect()
-        cursor = conn.cursor(pymysql.cursors.DictCursor)
-        sql = "SELECT team.name, team_stats.but, team_stats.passe, team_stats.km, team_stats.possesion, team_stats.date FROM team INNER JOIN team_stats ON team_stats.team_id=team.id AND team.id="+ id +"ORDER BY team_stats.date DES"
-        cursor.execute(sql)
->>>>>>> 993db78dcaa7e5683db8964e36067d7def262f6f
         rows = cursor.fetchall()
         resp = jsonify(rows)
         resp.status_code = 200
