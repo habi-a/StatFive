adf0ef5b3579446f9e93b128d679d78ddba96f6d
nassir_n <nassir_n@etna-alternance.net>
2019-07-24 17:44:05 +0200

    class to create json by match name

--- API/routes/match.py
+++ API/routes/match.py
@@ -5,11 +5,18 @@ from db_config import mysql
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api, reqparse, inputs
 
+class generateSimpleJson():
+    #method to create json for one match
+    def create(self, arrays):
+        for array in arrays:
+            print(array)
+        return array
+
 class AllMatchByDate(Resource):
     def get(self):
         conn = mysql.connect()
         cursor = conn.cursor(pymysql.cursors.DictCursor)
-        cursor.execute('SELECT team_has_match_played.goals, team_has_match_played.possesion, team_has_match_played.color, team.name, match_played.duration FROM team_has_match_played INNER JOIN match_played ON team_has_match_played.match_id = match_played.id INNER JOIN team ON team_has_match_played.team_id = team.id')
+        cursor.execute('SELECT team_has_match_played.match_id ,team_has_match_played.goals, team_has_match_played.possesion, team_has_match_played.color, team.name, match_played.duration, match_played.name FROM team_has_match_played INNER JOIN match_played ON team_has_match_played.match_id = match_played.id INNER JOIN team ON team_has_match_played.team_id = team.id')
         rows = cursor.fetchall()
         if not rows:
             return jsonify({'about':'no matchs found'})
