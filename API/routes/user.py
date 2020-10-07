4708a8a6bb726bcdd167264fa90e5b84ee6dfe67
root <root@vps-7eb471c0.vps.ovh.net.novalocal>
2020-10-07 19:32:20 +0000

    Dashboard fix bug CORS

--- API/routes/user.py
+++ API/routes/user.py
@@ -6,21 +6,23 @@ from flask import Flask, request, jsonify
 from flask_restful import Resource, Api, reqparse, inputs
 from flaskext.mysql import MySQL
 
-class user(Resource):   
+class user(Resource):
     def post(self):
         try:
             parser = reqparse.RequestParser()
             parser.add_argument('email', type=inputs.regex('^[^\W][a-zA-Z0-9_]+(\.[a-zA-Z0-9_]+)*\@[a-zA-Z0-9_]+(\.[a-zA-Z0-9_]+)*\.[a-zA-Z]{2,4}$'), required=True, help='Email address to create user')
-            parser.add_argument('password', type=str, required=True, help='Password to create user')
-            parser.add_argument('c_password', type=str, required=True, help='Confirm password to create user')
-            parser.add_argument('lastname', type=str, required=True, help='Lastname')
-            parser.add_argument('firstname', type=str, required=True, help='Firstname')
+            parser.add_argument('password', required=True, type=str, help='Password to create user')
+            parser.add_argument('c_password', required=True, type=str, help='Confirm password to create user')
+            parser.add_argument('lastname', required=True, type=str, help='Lastname')
+            parser.add_argument('firstname', required=True, type=str, help='Firstname')
             args = parser.parse_args()
+            print(args)
             email = args['email']
             password = hashlib.sha256(args['password'].encode()).hexdigest()
             password2 =  hashlib.sha256(args['c_password'].encode()).hexdigest()
             firstname = args['firstname']
             lastname = args['lastname']
+            role = 0
             if password == password2:
                 conn = mysql.connect()
                 cursor = conn.cursor(pymysql.cursors.DictCursor)
@@ -28,17 +30,17 @@ class user(Resource):
                 cursor.execute(sqlQuery)
                 rows = cursor.fetchall()
                 if len(rows) == 0:
-                    sqlQuery = 'INSERT INTO users ( mail, name, firstname, password) VALUES ("{}", "{}", "{}", "{}")'.format(email, lastname, firstname, password)
+                    sqlQuery = 'INSERT INTO users ( mail, name, firstname, password, role) VALUES ("{}", "{}", "{}", "{}", "{}")'.format(email, lastname, firstname, password, role)
                     cursor.execute(sqlQuery)
                     conn.commit()
-                    return jsonify({'Email': args['email'], 'Firstame':args['firstname'], 'Lastname':args['lastname'], 'status':200})
+                    return jsonify({'Email': args['email'], 'Role': role, 'Firstame':args['firstname'], 'Lastname':args['lastname'], 'status':200})
                 else:
                     return jsonify({'about':'Mail exist'})
             return jsonify({'about':'Password invalid'})
-            
+
         except Exception as e:
             return {'error' : str(e)}
-    
+
 class AllUser(Resource):
     def get(self):
         conn = mysql.connect()
@@ -53,7 +55,7 @@ class userByName(Resource):
     def get(self, name):
         conn = mysql.connect()
         cursor = conn.cursor(pymysql.cursors.DictCursor)
-        cursor.execute('SELECT firstname, lastname, mail FROM users WHERE firstname LIKE %s', name+'%')
+        cursor.execute('SELECT firstname, lastname, mail, role FROM users WHERE firstname LIKE %s', name+'%')
         rows = cursor.fetchall()
         resp = jsonify(rows)
         resp.status_code = 200
@@ -63,7 +65,7 @@ class userById(Resource):
     def get(self, id):
         conn = mysql.connect()
         cursor = conn.cursor(pymysql.cursors.DictCursor)
-        cursor.execute('SELECT firstname, lastname, mail FROM users WHERE id ={}').format(id)
+        cursor.execute('SELECT firstname, lastname, mail, role FROM users WHERE id ={}').format(id)
         rows = cursor.fetchall()
         resp = jsonify(rows)
         resp.status_code = 200
@@ -79,12 +81,12 @@ class login(Resource):
         password = hashlib.sha256(args['password'].encode()).hexdigest()
         conn = mysql.connect()
         cursor = conn.cursor(pymysql.cursors.DictCursor)
-        sql ='SELECT firstname, name, mail FROM users WHERE  mail= "{}" AND password = "{}"'.format(email, password)
+        sql ='SELECT firstname, name, mail, role FROM users WHERE  mail= "{}" AND password = "{}"'.format(email, password)
         cursor.execute(sql)
         rows = cursor.fetchall()
         if not rows:
-            error = {'about': 'Error Connection', 'Connected':False}
+            error = {'about': 'Error Connection', 'Connected':False, 'email': email, 'password': args['password']}
             return jsonify(error)
-        co= {'Connected':True}
+        co= {'Connected':True, 'User':rows}
         resp = jsonify(co)
         return resp
