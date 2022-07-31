644a4d6eb8e14c669ef2c98df719b620facf5324
Jaouad Elhormi <jaouad.elhormi@ajeel.fr>
2022-07-31 20:17:27 +0200

    url

--- api/src/helper/__init__.py
+++ api/src/helper/__init__.py
@@ -15,5 +15,5 @@ def custom_response(res, status_code):
 
 
 def video_url_for(func: str, **kwargs):
-    url = os.environ.get('URL', 'http://127.0.0.1:5000')
+    url = os.environ.get('URL', 'http://api:5000')
     return f"{url}{url_for(func, **kwargs)}"
