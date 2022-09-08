07b479a3de8cb76b2cfb51ee3ab4939fc0260894
Mokhtari Sofiane <mokhta_s@etna-alternance.net>
2022-09-08 16:04:03 +0200

    fix login

--- web/pages/index.js
+++ web/pages/index.js
@@ -49,11 +49,11 @@ export default function Home() {
       if(res.data.error) {
         setError(res.data.message)
       } else {
-        addToken(res.data.data.token);
-        addVerif(res.data.data.verification);
-        addValue(res.data.data.id)
-        addData(res.data.data)
-        if(res.data.data.verification) {
+        addToken(res.data.token);
+        addVerif(res.data.verification);
+        addValue(res.data.id)
+        addData(res.data)
+        if(res.data.verification) {
           router.push("/accueil"); 
           setCheck()
         } else {
