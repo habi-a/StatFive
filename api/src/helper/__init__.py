225e3ada895410f2bd79043649fa809de006b593
root <root@vm094.courta_f-gpe-2022.etna.local>
2022-09-09 00:33:41 +0200

    fix mail sender

--- api/src/helper/__init__.py
+++ api/src/helper/__init__.py
@@ -15,5 +15,5 @@ def custom_response(res, status_code):
 
 
 def video_url_for(func: str, **kwargs):
-    url = os.environ.get('URL', 'https://api.preprod.statfive.fr')
+    url = os.environ.get('API_URL', 'https://api.statfive.fr')
     return f"{url}{url_for(func, **kwargs)}"
